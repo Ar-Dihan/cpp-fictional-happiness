fba30dc96b513ef784099dabde50fd3aa415b9a8
Ar-Dihan <arnahid18@gmail.com>
2022-03-15 20:44:59 +0600

    input output

---
+++ code.cpp
@@ -0,0 +1,16 @@
+// Program to print positive number entered by the user
+// If the user enters a negative number, it is skipped
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int amount1;
+  int amount2;
+  cin >> amount1;
+  cin >> amount2;
+  int total;
+  total = amount1 + amount2;
+  cout << total;
+  return 0;
+}
---
+++ codee/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "amann" << endl;
+    int amount1;
+    int amount2;
+    cin >> amount1;
+    cin >> amount2;
+    int total;
+    total = amount1 + amount2;
+    cout << total;
+    return 0;
+}
