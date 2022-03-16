61c0e3ebde488051948268b2f524a25bb38faa83
Ar-Dihan <arnahid18@gmail.com>
2022-03-16 23:51:28 +0600

    cpp-fictional-happiness

--- codee/hello.cpp
+++ codee/hello.cpp
@@ -151,3 +151,31 @@ int main()
     cout << outpot;
     return 0;
 }
+
+/*
+A robot can type 300 letters per minute.
+The given program takes the text input time (in minutes).
+
+Task
+Complete the code to calculate and output the number of letters that will be typed in a given time.
+
+Sample Input
+20
+
+Sample Output
+6000
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 300; // letter in a minute
+    int time;
+    cin >> time;
+
+    // your code goes here
+    count *= time;
+    cout << count << endl;
+    return 0;
+}
