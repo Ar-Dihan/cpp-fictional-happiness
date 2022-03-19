cf3a75d9c4b48972b05d4c97f91d8a3defbcfd97
Ar-Dihan <arnahid18@gmail.com>
2022-03-19 22:02:08 +0600

    half done

--- free-code-camp/fcc.cpp
+++ free-code-camp/fcc.cpp
@@ -2,6 +2,45 @@
 using namespace std;
 int main()
 {
+    /*
+    cout << "       *" << endl;
+    cout << "      **" << endl;
+    cout << "     ***" << endl;
+    cout << "    ****" << endl;
+    cout << "   *****" << endl;
+    cout << "  ******" << endl;
+    cout << " *******" << endl;
+    cout << "********" << endl;
+    */
+
+    //  data types
+    /*
+        char letters = 'A';
+        string sentence = "this should be in double qoute";
+        int eveteager = 69;
+        float goat = 69.69;
+        double trouble = 69.69;
+        bool cool = true;
+        */
+
+    // string stuff
+    /*
+        string sentence = "useless stuffs";
+
+        // getting length
+         cout << sentence.length();
+
+        // getting idividual char
+         cout << sentence[5];
+
+        // getting index of a string or char
+         cout << sentence.find("stuffs");
+
+        // making suvstring from a string
+        cout << sentence.substr(2, 4);
+    */
+
+    // numbers
 
     return 0;
 }
