6dbe736918f26ae23a88baf5e6756f686b43a52d
Ar-Dihan <arnahid18@gmail.com>
2022-03-23 17:02:14 +0600

    done

--- free-code-camp/fcc.cpp
+++ free-code-camp/fcc.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-/*
+
 void saybye(string name, int age)
 {
     cout << "tata " << name << "\nyou are " << age;
 }
-*/
-/*
+
 int cube(int num)
 {
     int result = num * num * num;
     return result;
 }
-*/
 
 class car
 {
@@ -26,7 +24,7 @@ public:
 
 int main()
 {
-    /*
+
     cout << "       *" << endl;
     cout << "      **" << endl;
     cout << "     ***" << endl;
@@ -35,65 +33,60 @@ int main()
     cout << "  ******" << endl;
     cout << " *******" << endl;
     cout << "********" << endl;
-    */
 
     //  data types
-    /*
-        char letters = 'A';
-        string sentence = "this should be in double qoute";
-        int eveteager = 69;
-        float goat = 69.69;
-        double trouble = 69.69;
-        bool cool = true;
-        */
+
+    char letters = 'A';
+    string sentence = "this should be in double qoute";
+    int eveteager = 69;
+    float goat = 69.69;
+    double trouble = 69.69;
+    bool cool = true;
 
     // string stuff
-    /*
-        string sentence = "useless stuffs";
 
-        // getting length
-         cout << sentence.length();
+    string sentence = "useless stuffs";
 
-        // getting idividual char
-         cout << sentence[5];
+    // getting length
+    cout << sentence.length();
 
-        // getting index of a string or char
-         cout << sentence.find("stuffs");
+    // getting idividual char
+    cout << sentence[5];
 
-        // making suvstring from a string
-        cout << sentence.substr(2, 4);
-    */
+    // getting index of a string or char
+    cout << sentence.find("stuffs");
+
+    // making suvstring from a string
+    cout << sentence.substr(2, 4);
 
     // numbers
-    /*
+
     cout << pow(3, 3);
     cout << sqrt(25);
     cout << fmax(69, 59) << endl
-        << fmin(69, 96);
-*/
+         << fmin(69, 96);
 
     // input
-    /*
-        cout <<"Enter your name: " << endl;
-        string name;
-        getline(cin, name);
-        cout << "Get lost " << name;
-    */
-    // is_array
-    /*
-        int is_array[] = {65, 6565, 556, 5366, 5, 56, 55};
 
-        cout << is_array;
-    */
+    cout << "Enter your name: " << endl;
+    string name;
+    getline(cin, name);
+    cout << "Get lost " << name;
+
+    // is_array
+
+    int is_array[] = {65, 6565, 556, 5366, 5, 56, 55};
+
+    cout << is_array;
 
     // Function
-    /*
-        saybye("coders", 6565);
-        return 0;
-    */
-    //  cout << cube(5);
+
+    saybye("coders", 6565);
+    return 0;
+
+    cout << cube(5);
+
     // if else
-    /*
     string name;
     getline(cin, name);
     bool isMale = true;
@@ -102,105 +95,103 @@ int main()
         if (name == "atik")
             cout << name << " is a sigma male";
     }
-    */
-    // awesosme calculator;
-    /*
-        int num1, num2;
-        char op;
-        cout << "enter first number: ";
-        cin >> num1;
-        cout << "enter operator: ";
-        cin >> op;
-        cout << "enter second number: ";
-        cin >> num2;
 
-        if (op == '+')
-        {
-            cout << "your answer is : " << num1 + num2;
-        }
-        else if (op == '-')
-        {
-            cout << "your answer is : " << num1 - num2;
-        }
-        else if (op == '*')
-        {
-            cout << "your answer is : " << num1 * num2;
-        }
-        else if (op == '/')
-        {
-            cout << "your answer is : " << num1 / num2;
-        }
-        else
-        {
-            cout << "something was wrong :( ";
-        }
-    */
+    // awesosme calculator;
+
+    int num1, num2;
+    char op;
+    cout << "enter first number: ";
+    cin >> num1;
+    cout << "enter operator: ";
+    cin >> op;
+    cout << "enter second number: ";
+    cin >> num2;
+
+    if (op == '+')
+    {
+        cout << "your answer is : " << num1 + num2;
+    }
+    else if (op == '-')
+    {
+        cout << "your answer is : " << num1 - num2;
+    }
+    else if (op == '*')
+    {
+        cout << "your answer is : " << num1 * num2;
+    }
+    else if (op == '/')
+    {
+        cout << "your answer is : " << num1 / num2;
+    }
+    else
+    {
+        cout << "something was wrong :( ";
+    }
+
     // while loop
-    /*
-        int i = 0;
-        int x;
-        cin >> x;
-        while (i <= x)
-        {
-            cout << i << endl;
-            i++;
-        }
-    */
+
+    int i = 0;
+    int x;
+    cin >> x;
+    while (i <= x)
+    {
+        cout << i << endl;
+        i++;
+    }
 
     // Number Guessing Game
-    /*
-        cout << "Guess A number";
-        cout << "Now Multiply this with 2";
-        cout << "Now add 10 with multiplication";
-        cout << "Now divide the number by 2";
-        cout << "minus your guess number";
 
-        cout << "you guess 5";
-        */
+    cout << "Guess A number";
+    cout << "Now Multiply this with 2";
+    cout << "Now add 10 with multiplication";
+    cout << "Now divide the number by 2";
+    cout << "minus your guess number";
+
+    cout << "you guess 5";
+
     // for loop
-    /*
-        for (int i = 0; i < 5; i++)
-        {
-            cout << "hello there \n";
-        }
-        */
 
-    // while loop
-    /*
-        int x = 0;
-        int in;
-        cin >> in;
-        while (x < in)
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "hello there \n";
+    }
+
+    while
+        loop
+
+            int x = 0;
+    int in;
+    cin >> in;
+    while (x < in)
+    {
+        cout << x << endl;
+        if (x % 4 == 0)
         {
-            cout << x << endl;
-            if (x % 4 == 0)
-            {
-                cout << "working\n";
-            }
-            x++;
+            cout << "working\n";
         }
-    */
-    // multiple array
-    /*
+        x++;
+    }
+
+    multiple array
+
         int myArray[2][3] = {
             {2, 4, 3},
             {3, 5, 2}};
 
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 3; j++)
-                cout << myArray[i][j]
-                     << endl;
-        }
-    */
-    // pointer
-    /*
-        int p = 33;
-        cout << &*&p;
-    */
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            cout << myArray[i][j]
+                 << endl;
+    }
 
-    // class & object
-    car car1;
+    pointer
+
+        int p = 33;
+    cout << &*&p;
+
+    class &object
+        car car1;
     cout << "enter car name: ";
     cin >> car1.name;
     cout << "enter color: ";
@@ -208,14 +199,15 @@ int main()
     cout << "enter milage: ";
     cin >> car1.horse_pow;
     cout << "enter horse-power: ";
-    // car1.name = "fly";
-    // car1.color = "green";
-    // car1.milage = 43;
-    // car1.horse_pow = 828;
 
-    // cout << car1.name << endl
-    //      << car1.color << endl
-    //      << car1.milage << endl
-    //      << car1.horse_pow;
+    car1.name = "fly";
+    car1.color = "green";
+    car1.milage = 43;
+    car1.horse_pow = 828;
+
+    cout << car1.name << endl
+         << car1.color << endl
+         << car1.milage << endl
+         << car1.horse_pow;
     return 0;
 }
